8a6fc876d0fb7996d0e9b794b00ded31f738c7d6
Victor Eleanya <victoreleanya89@gmail.com>
2025-05-17 20:29:38 +0100

    feat(linkedList.ts): added new methods to linkedList class

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,20 @@ const linkedList = new LinkedList<number>();
 linkedList.append(10);
 linkedList.append(20);
 linkedList.prepend(5);
+linkedList.append(6);
+linkedList.append(10);
+linkedList.append(4);
+linkedList.append(10);
 linkedList.insertAt(15, 2);
 
+linkedList.print();
+
+linkedList.append(5);
+linkedList.append(2);
+linkedList.append(13);
+linkedList.append(3);
+linkedList.append(8);
+
+linkedList.print();
+linkedList.partition(5);
 linkedList.print();
--- src/linkedList.ts
+++ src/linkedList.ts
@@ -1,3 +1,5 @@
+import { count } from "console";
+
 export class Node<T> {
   value: T;
   next: Node<T> | null;
@@ -45,26 +47,23 @@ export class LinkedList<T> {
     this.size++;
   }
 
-  insertAt(value: T, index: number): boolean {
-    if (index < 0 || index > this.size) {
-      return false;
-    }
+  insertAt(index: number, value: T): void {
+    if (index < 0 || index > this.size) return;
     if (index === 0) {
       this.prepend(value);
-      return true;
     }
     if (index === this.size) {
       this.append(value);
-      return true;
     }
 
     const newNode = new Node(value);
-    let current = this.head;
+    let current: Node<T> | null = this.head;
     let previous: Node<T> | null = null;
     let position = 0;
-    while (position < index) {
+
+    while (position < index && current) {
       previous = current;
-      current = current?.next || null;
+      current = current.next;
       position++;
     }
 
@@ -72,20 +71,162 @@ export class LinkedList<T> {
       newNode.next = current;
       previous.next = newNode;
       this.size++;
-      return true;
+    }
+  }
+
+  removeAt(index: number): void {
+    if (index < 0 || index > this.size || !this.head) return;
+
+    if (index === 0) {
+      this.head = this.head?.next;
+
+      if (this.size === 1) {
+        this.tail = null;
+      }
+      this.size--;
     }
 
-    return false;
+    let current: Node<T> | null = this.head;
+    let previous: Node<T> | null = null;
+    let position = 0;
+
+    while (position < index && current) {
+      previous = current;
+      current = current.next;
+
+      position++;
+    }
+
+    if (previous && current) {
+      previous.next = current.next;
+
+      if (index === this.size - 1) {
+        this.tail = previous;
+      }
+      this.size--;
+    }
+  }
+
+  remove(value: T): void {
+    if (!this.head) return;
+
+    if (value === this.head.value) {
+      this.head = this.head.next;
+
+      if (this.size === 1) {
+        this.tail = null;
+      }
+      this.size--;
+    }
+
+    let current: Node<T> | null = this.head;
+    let previous: Node<T> | null = null;
+    let position = 0;
+
+    while (current && current.value !== value) {
+      previous = current;
+      current = current.next;
+
+      position++;
+    }
+
+    if (!current) return;
+
+    if (previous) {
+      previous.next = current.next;
+
+      if (current === this.tail) {
+        this.tail = previous;
+      }
+      this.size--;
+    }
+  }
+
+  removeDuplicates(): void {
+    if (!this.head) return;
+    let current: Node<T> | null = this.head;
+    let previous: Node<T> | null = null;
+    const hasSeen = new Set<unknown>();
+
+    while (current) {
+      if (hasSeen.has(current.value)) {
+        if (previous) {
+          previous.next = current.next;
+
+          if (current === this.tail) {
+            this.tail = previous;
+          }
+        }
+        current = current.next;
+        this.size--;
+      } else {
+        hasSeen.add(current.value);
+        previous = current;
+        current = current.next;
+      }
+    }
+  }
+
+  removeNodes(head: Node<T>): Node<T> | null {
+    if (!head || head.next === null) return head;
+    let nextNode = this.removeNodes(head.next);
+    if (nextNode) {
+      if (nextNode?.value > head.value) {
+        return nextNode;
+      }
+    }
+    head.next = nextNode;
+    return head;
+  }
+
+  returnKthToLast(k: number): T | null {
+    if (k <= 0 || k > this.size || !this.head) return null;
+    let current: Node<T> | null = this.head;
+    let targetPosition = this.size - k;
+    let position = 0;
+    while (position < targetPosition && current) {
+      current = current.next;
+      position++;
+    }
+
+    return current ? current.value : null;
+  }
+
+  partition(value: T): void {
+    if (!this.head) return;
+    let beforeDummy = new Node<T>(null as any);
+    let afterDummy = new Node<T>(null as any);
+
+    let before: Node<T> | null = beforeDummy;
+    let after: Node<T> | null = afterDummy;
+
+    let current: Node<T> | null = this.head;
+    while (current) {
+      const nextNode: Node<T> | null = current.next;
+
+      current.next = null;
+
+      if (current.value > value) {
+        after.next = current;
+        after = current;
+      } else {
+        before.next = current;
+        before = current;
+      }
+
+      current = nextNode;
+    }
+    before.next = afterDummy.next;
+    this.head = beforeDummy.next;
   }
 
   print(): void {
     let current = this.head;
     const values: T[] = [];
-
     while (current) {
       values.push(current.value);
       current = current.next;
     }
-    console.log(values.join(" -> "));
+    console.log(values.join("->"));
   }
 }
